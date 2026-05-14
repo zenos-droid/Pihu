1130671e4b94bbe8a3725ef7b401ebc7b2f56060
Ayush <ayushgore21@gmail.com>
2026-05-15 04:17:03 +0530

    Add files via upload

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 import { createServer as createViteServer } from "vite";
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 const DB_FILE = path.join(process.cwd(), "passes-old.json");
 
 // -- DIRECT DATABASE SCHEMATICS & INITIALIZERS --
